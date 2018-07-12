34519afcc3b3ed041577f734aed9f8e5b724f761
DominionEbenezar <dominionebenezar123@gmail.com>
2018-07-12 08:04:55 -0700

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function (){
     console.log('connected to server');
-
-    socket.emit('createMessage', {
-       from: 'Dominion',
-        text: 'Hey this is dominion'
-    });
 });
 
 socket.on('disconnect', function () {
