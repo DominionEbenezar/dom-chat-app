139727b652b2feb92349df90e3d0e62c04af0cd7
DominionEbenezar <dominionebenezar123@gmail.com>
2018-07-14 07:46:06 -0700

    Adding css style to the work

--- public/js/index.js
+++ public/js/index.js
@@ -19,7 +19,7 @@ socket.on('newMessage', function(message){
     socket.on('newLocationMessage', function(message){ 
         var li = jQuery('<li></li>');
         var a = jQuery('<a target="_blank">My current location</a>');
-        
+
         li.text(`${message.from}: `);
         a.attr('href', message.url);
         li.append(a);
@@ -29,11 +29,13 @@ socket.on('newMessage', function(message){
   jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
+    var messageTextBox =  jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextBox.val()
     }, function (){
-
+     messageTextBox.val('')
     });
 
 });
@@ -44,12 +46,16 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported by your browwer.');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location..');
+
     navigator.geolocation.getCurrentPosition(function (position) {
-            socket.emit('createLocationMessage', {
+        locationButton.removeAttr('disabled').text('Send location');
+        socket.emit('createLocationMessage', {
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude
             });
     }, function() {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('unable to fetch location');
     });
 });
--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ console.log('New User connected');
 socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-    callback('This is from the server');
+    callback();
 });
 
 socket.on('createLocationMessage',(coords) =>{
