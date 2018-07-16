fe460d35528fdf3acaa876c4c62ad9c3c55ff451
DominionEbenezar <dominionebenezar123@gmail.com>
2018-07-16 16:09:17 -0700

    last commit

--- server/server.js
+++ server/server.js
@@ -44,7 +44,10 @@ socket.on('join', (params, callback) =>{
 });
 
 socket.on('createLocationMessage',(coords) =>{
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    var user = users.getUser(socket.id);
+    if(user){
+        io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
 });
 
 socket.on('disconnect', () =>{
